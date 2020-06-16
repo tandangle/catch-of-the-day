84e63506a6bac6de284119d3b0285854bfde66d3
Tandangle <tandangle76@gmail.com>
2020-06-16 19:13:18 -0400

    removing console logs

--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -1,105 +1,96 @@
-import React from "react";
-import PropTypes from "prop-types";
-import firebase from "firebase";
-import base, { firebaseApp } from "../base";
+import React from 'react';
+import PropTypes from 'prop-types';
+import firebase from 'firebase';
+import base, { firebaseApp } from '../base';
 
-import AddFishForm from "./AddFishForm" 
-import EditFishForm from "./EditFishForm"
-import Login from "./Login"
+import AddFishForm from './AddFishForm';
+import EditFishForm from './EditFishForm';
+import Login from './Login';
 
 class Inventory extends React.Component {
+	static propTypes = {
+		fishes: PropTypes.object,
+		updateFish: PropTypes.func,
+		deleteFish: PropTypes.func,
+		addFish: PropTypes.func,
+		loadSampleFishes: PropTypes.func
+	};
 
-    static propTypes = {
-        fishes: PropTypes.object,
-        updateFish: PropTypes.func,
-        deleteFish: PropTypes.func,
-        addFish: PropTypes.func,
-        loadSampleFishes: PropTypes.func
-    };
+	state = {
+		uid: null,
+		owner: null
+	};
 
-    state = {
-        uid: null,
-        owner: null
-    }
+	componentDidMount() {
+		firebase.auth().onAuthStateChanged((user) => {
+			if (user) {
+				this.authHandler({ user });
+			}
+		});
+	}
 
-    componentDidMount() {
-        firebase.auth().onAuthStateChanged(user => {
-            if(user) {
-                this.authHandler({ user });
-            }
-        })
-    }
+	authHandler = async (authData) => {
+		// 1. Look up the current store in the Firebase DB
+		const store = await base.fetch(this.props.storeId, { context: this });
+		// 2. Claim the store if there is no owner
+		if (!store.owner) {
+			await base.post(`${this.props.storeId}/owner`, {
+				data: authData.user.uid
+			});
+		}
+		// 3. Set state of inventory component to reflect current user
+		this.setState({
+			uid: authData.user.uid,
+			owner: store.owner || authData.user.uid
+		});
+	};
 
-    authHandler = async authData => {
-        // 1. Look up the current store in the Firebase DB
-        const store = await base.fetch(this.props.storeId, { context: this });
-        console.log(store);
-        // 2. Claim the store if there is no owner
-        if (!store.owner) {
-            await base.post(`${this.props.storeId}/owner`, {
-                data: authData.user.uid
-            })
-        }
-        // 3. Set state of inventory component to reflect current user
-        this.setState({
-            uid: authData.user.uid,
-            owner: store.owner || authData.user.uid
-        })
+	authenticate = (provider) => {
+		const authProvider = new firebase.auth[`${provider}AuthProvider`]();
+		firebaseApp.auth().signInWithPopup(authProvider).then(this.authHandler);
+	};
 
-        console.log(authData);
-    }
+	logout = async () => {
+		await firebase.auth().signOut();
+		this.setState({
+			uid: null
+		});
+	};
 
-    authenticate = (provider) => {
-        const authProvider = new firebase.auth[`${provider}AuthProvider`]();
-        firebaseApp
-            .auth()
-            .signInWithPopup(authProvider)
-            .then(this.authHandler)
-    }
+	render() {
+		const logout = <button onClick={this.logout}>Log Out!</button>;
 
-    logout = async () => {
-        console.log('Logging out!');
-        await firebase.auth().signOut();
-        this.setState({
-            uid: null
-        })
-    }
+		if (!this.state.uid) {
+			return <Login authenticate={this.authenticate} />;
+		}
 
-    render(){
+		if (this.state.uid != this.state.owner) {
+			return (
+				<div>
+					<p>Sorry, you are not the owner</p>
+					{logout}
+				</div>
+			);
+		}
 
-        const logout = <button onClick={this.logout}>Log Out!</button>
-
-        if(!this.state.uid) {
-            return <Login authenticate={this.authenticate}/>
-        }
-
-        if(this.state.uid != this.state.owner) {
-            return <div>
-                <p>
-                Sorry, you are not the owner    
-                </p>
-                {logout}
-            </div>
-        }
-
-        return (
-           <div className="inventory">
-               <h2>Inventory!</h2>
-               {logout}
-               {Object.keys(this.props.fishes).map(key => (
-               <EditFishForm 
-                key={key} 
-                index={key} 
-                fish={this.props.fishes[key]} 
-                updateFish={this.props.updateFish} 
-                deleteFish={this.props.deleteFish}
-                />
-                ))}
-               <AddFishForm addFish={this.props.addFish} />
-                <button onClick={this.props.loadSampleFishes}> Load Sample Fishes</button>
-           </div>
-        );
-    }
+		return (
+			<div className="inventory">
+				<h2>Inventory!</h2>
+				{logout}
+				{Object.keys(this.props.fishes).map((key) => (
+					<EditFishForm
+						key={key}
+						index={key}
+						fish={this.props.fishes[key]}
+						updateFish={this.props.updateFish}
+						deleteFish={this.props.deleteFish}
+					/>
+				))}
+				<AddFishForm addFish={this.props.addFish} />
+				<button onClick={this.props.loadSampleFishes}> Load Sample Fishes</button>
+			</div>
+		);
+	}
 }
 
 export default Inventory;
